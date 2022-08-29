c8614bc54e7e22c5a2aae0f8d4fe9f3738fa3859
Thauannebyte <111437556+Thauannebyte@users.noreply.github.com>
2022-08-29 20:37:03 -0300

    Update webbb.py

--- webbb.py
+++ webbb.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import numpy as np
-import plotly as go
+import plotly.express as px
 
 t = np.linspace(0, 0.05, 1000)
 v = 220*np.sqrt(2)*np.sin(2*np.pi*60*t)
 c = (220*np.sqrt(2)*np.sin(2*np.pi*60*t))*2
 
-fig1 = go.Figure(data=go.Scatter(x=t, y=v))
-fig2 = go.Figure(data=go.Scatter(x=t, y=c))
+fig1 = px.Figure(data=go.Scatter(x=t, y=v))
+fig2 = px.Figure(data=go.Scatter(x=t, y=c))
 
 
 st.markdown("# Tarefa Primeira Etapa WebApp")
